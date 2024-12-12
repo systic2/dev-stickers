799aab9a40d9453b91c6b643cee1b464cbaed9f5
systic <llsysticll@gmail.com>
2024-12-12 18:51:37 +0900

    Blog Post

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { graphql } from "gatsby";
+import Seo from "../../components/Seo";
 
-export default function BlogPost() {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div />
+    <Layout title="">
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      body
+      frontmatter {
+        author
+        category
+        date
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export const Head = ({ data }: IBlogPostProps) => (
+  <Seo title={data.mdx?.frontmatter?.title} />
+);
