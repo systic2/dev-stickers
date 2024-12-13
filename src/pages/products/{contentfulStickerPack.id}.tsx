9c45f7a22be8bd3654ceac2bbdbc3466c5fd1854
systic <llsysticll@gmail.com>
2024-12-13 19:43:33 +0900

    Product Detail

--- src/pages/products/{contentfulStickerPack.id}.tsx
+++ src/pages/products/{contentfulStickerPack.id}.tsx
@@ -1,4 +1,28 @@
 import React from "react";
-export default function ProductDetail() {
-  return <h1>hi</h1>;
+import Layout from "../../components/Layout";
+import { graphql, PageProps } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+export default function ProductDetail({
+  data,
+}: PageProps<Queries.ProductQuery>) {
+  const image = getImage(data.contentfulStickerPack?.preview?.gatsbyImageData!);
+  return (
+    <Layout title={data.contentfulStickerPack?.name!}>
+      <GatsbyImage image={image!} alt={data.contentfulStickerPack?.name!} />
+      <h2>${data.contentfulStickerPack?.price}</h2>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+  query Product($id: String!) {
+    contentfulStickerPack(id: { eq: $id }) {
+      name
+      price
+      preview {
+        gatsbyImageData(height: 450, placeholder: BLURRED)
+      }
+    }
+  }
+`;
