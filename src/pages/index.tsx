ea2c66868580c6b88c06e00325a1b379265651b8
systic <llsysticll@gmail.com>
2024-12-13 19:30:11 +0900

    Contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,38 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { graphql, PageProps } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-export default function IndexPage() {
+export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
   return (
     <Layout title="Welcome to DevStickers 👏">
-      <div></div>
+      {data.allContentfulStickerPack.nodes.map((sticker) => (
+        <article>
+          <GatsbyImage
+            image={getImage(sticker.preview?.gatsbyImageData!)!}
+            alt={sticker.name!}
+          />
+          <h2>{sticker.name}</h2>
+          <h4>${sticker.price}</h4>
+        </article>
+      ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query Stickers {
+    allContentfulStickerPack {
+      nodes {
+        name
+        price
+        preview {
+          gatsbyImageData(placeholder: BLURRED, height: 250)
+        }
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Home" />;
---
+++ src/pages/products/{contentfulStickerPack.id}.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+export default function ProductDetail() {
+  return <h1>hi</h1>;
+}
